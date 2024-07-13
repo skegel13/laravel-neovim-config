e8504267f1bc03478ad982dff60dc56f99e05c3e
Sean Kegel <me@seankegel.com>
2024-07-12 23:11:48 -0400

    Add Laravel plugin

--- nvim/lua/plugins/php.lua
+++ nvim/lua/plugins/php.lua
@@ -22,4 +22,24 @@ return {
     dependencies = { "V13Axel/neotest-pest" },
     opts = { adapters = { "neotest-pest" } },
   },
+  {
+    "adalessa/laravel.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "tpope/vim-dotenv",
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+    keys = {
+      { "<leader>la", ":Laravel artisan<cr>" },
+      { "<leader>lr", ":Laravel routes<cr>" },
+      { "<leader>lm", ":Laravel related<cr>" },
+    },
+    event = { "VeryLazy" },
+    config = true,
+    opts = {
+      lsp_server = "intelephense",
+      features = { null_ls = { enable = false } },
+    },
+  },
 }
