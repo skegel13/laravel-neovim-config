0cda44fc580214d6c1965c9ef5b3dc10c0d3efbb
Sean Kegel <me@seankegel.com>
2024-07-13 15:22:57 -0400

    Add Blade syntax highlighting

--- nvim/lua/plugins/php.lua
+++ nvim/lua/plugins/php.lua
@@ -49,4 +49,31 @@ return {
     },
     ft = { "blade", "php" }, -- optional, improves startup time
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "blade",
+        "php_only",
+      })
+    end,
+    config = function(_, opts)
+      vim.filetype.add({
+        pattern = {
+          [".*%.blade%.php"] = "blade",
+        },
+      })
+
+      require("nvim-treesitter.configs").setup(opts)
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
+    end,
+  },
 }
